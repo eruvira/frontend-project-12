46a7fa2dd755823a3dd092979b2d45103b8ddb2f
Elvira Darisheva <d.elvira@rocketfirm.com>
2025-06-29 20:46:22 +0500

    feat: add lint n prettier

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,29 +1,18 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import { defineConfig, globalIgnores } from 'eslint/config'
-
-export default defineConfig([
-  globalIgnores(['dist']),
-  {
-    files: ['**/*.{js,jsx}'],
-    extends: [
-      js.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-    ],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-      parserOptions: {
-        ecmaVersion: 'latest',
-        ecmaFeatures: { jsx: true },
-        sourceType: 'module',
-      },
-    },
-    rules: {
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
-    },
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
   },
-])
+  extends: [
+    'react-app',
+    'airbnb',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended', 
+  ],
+  plugins: ['react', 'react-hooks', 'prettier'],
+  rules: {
+    'react/jsx-filename-extension': [1, { extensions: ['.jsx'] }],
+    'prettier/prettier': ['error'],
+    'react/react-in-jsx-scope': 'off', 
+  },
+}
